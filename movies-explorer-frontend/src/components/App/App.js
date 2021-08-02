2ca0189745afdd95e13949470369b0111f330631
Kseniya St <kseniya7991@gmail.com>
2021-08-02 17:55:53 +0300

    add components, add Header component

---
+++ movies-explorer-frontend/src/components/App/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './App.css';
+/* import { Switch, Route, Link } from 'react-router-dom'; */
+
+//Импорт компонентов
+import Header from '../Header/Header';
+/*import Main from '../Main';
+import Movies from '../Movies';
+import SavedMovies from '../SavedMovies';
+import Profile from '../Profile';
+import Login from '../Login';
+import Register from '../Register';
+import Footer from '../Footer'; */
+
+function App() {
+  return (
+    <div className="App">
+       <Header />
+    {/* <Route path="/">
+        <Main />
+     </Route>
+     <Route path="/movies">
+        <Movies />
+     </Route>
+     <Route path="/saved-movies">
+        <SavedMovies />
+     </Route>
+     <Route path="/profile">
+        <Profile />
+     </Route>
+     <Route path="/signin">
+        <Login />
+     </Route>
+     <Route path="/signup">
+        <Register />
+     </Route>
+     <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ movies-explorer-frontend/src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../../images/logo.svg';
+import { Link } from 'react-router-dom';
+
+import './Header.css';
+
+function Header() {
+    return (
+        <header className="Header">
+            <img className="Header__logo" src={logo} alt="Фильмы" title="Фильмы" />
+            <div className="Header__sign-wrap">
+                <Link className="Header__signup" to="signup">Регистрация</Link>
+                <Link to="signin" className="Header__signin">Войти</Link>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
