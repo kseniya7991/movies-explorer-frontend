42da2bc020289e9aaf25e330e096cb61cfe44130
Kseniya St <kseniya7991@gmail.com>
2021-08-06 10:40:10 +0300

    complete Footer component

--- movies-explorer-frontend/src/components/Footer/Footer.js
+++ movies-explorer-frontend/src/components/Footer/Footer.js
@@ -2,18 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import './Footer.css';
+import '../Main/Main.css';
 
 function Footer() {
   return (
     <footer className="Footer">
-      <p>Учебный проект Яндекс.Практикум х BeatFilm.</p>
-      <hr />
-      <div>
-        <p>@ 2021</p>
-        <nav>
-          <Link to="https://praktikum.yandex.ru/">Яндекс.Практикум</Link>
-          <Link to="https://github.com/kseniya7991">GitHub</Link>
-          <Link to="https://www.linkedin.com/in/kseniya-stoychikova-907594201/">
+      <p className="Footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <hr className="Main__line Main__line_footer"/>
+      <div className="Footer__wrap-block">
+        <p className="Footer__year">&copy;2021</p>
+        <nav className="Footer__nav">
+          <Link className="Footer__nav-item" to="https://praktikum.yandex.ru/">Яндекс.Практикум</Link>
+          <Link className="Footer__nav-item" to="https://github.com/kseniya7991">GitHub</Link>
+          <Link className="Footer__nav-item" to="https://www.linkedin.com/in/kseniya-stoychikova-907594201/">
             LinkedIn
           </Link>
         </nav>
--- movies-explorer-frontend/src/components/Main/Main.js
+++ movies-explorer-frontend/src/components/Main/Main.js
@@ -5,6 +5,7 @@ import Promo from '../Promo/Promo';
 import AboutProject from '../AboutProject/AboutProject';
 import Techs from '../Techs/Techs';
 import AboutMe from '../AboutMe/AboutMe';
+import Footer from '../Footer/Footer';
 
 function Main() {
   return (
@@ -13,15 +14,7 @@ function Main() {
       <AboutProject />
       <Techs />
       <AboutMe />
-      {/*  <section className="Main__portfolio">
-        <h3>Портфолио</h3>
-        <hr />
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-      </section> */}
+      <Footer />
     </div>
   );
 }
