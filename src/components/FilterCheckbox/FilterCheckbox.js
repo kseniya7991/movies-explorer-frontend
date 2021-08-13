fa263b84785b3e76e5d65a1287fa469dccb84e62
Kseniya St <kseniya7991@gmail.com>
2021-08-13 15:33:20 +0300

    edit SearchForm component

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -5,10 +5,10 @@ import './FilterCheckbox.css';
 function FilterCheckbox() {
   return (
     <div className="filterCheckbox">
-      <div className="filterCheckbox__switch">
+      <label className="filterCheckbox__switch">
         <input className="filterCheckbox__checkbox" type="checkbox"></input>
         <span className="filterCheckbox__slider"></span>
-      </div>
+      </label>
       <label className="filterCheckbox__switch-description">
         Короткометражки
       </label>
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 
+import '../SignForm/SignForm.css';
 import './SearchForm.css';
 
 function SearchForm() {
@@ -11,10 +12,10 @@ function SearchForm() {
   const isValid = true;
 
   /*   const onSubmit = (data) => console.log(data); */
-  /*   const tester = () => console.log('smth'); */
+  const tester = () => console.log('smth');
 
   return (
-    <form className="searchForm" id="searchForm">
+<form className="searchForm" onSubmit={tester}>
       <div className="searchForm__search-block">
         <input
         name="searchField"
@@ -31,7 +32,6 @@ function SearchForm() {
             type="submit"
             className="searchForm__find-btn"
             value="1"
-            form="searchForm"
           ></button>
         </div>
       </div>
@@ -45,3 +45,33 @@ export default SearchForm;
 /* {errorMessage && !isValid && (
   <span className="error">{errorMessage}</span>
 )} */
+
+/*    <form className="searchForm" id="searchForm">
+      <div className="searchForm__search-block">
+        <input
+        name="searchField"
+          id="searchField"
+          className="searchForm__search"
+          type="text"
+          placeholder="Фильм"
+        ></input>
+        {errorMessage && !isValid && (
+          <span className="searchForm__text-error">{errorMessage}</span>
+        )}
+                  <button
+            type="submit"
+            className="searchForm__find-btn"
+            value="1"
+            form="searchForm"
+          ></button>
+        <div className="searchForm__find-block">
+          <button
+            type="submit"
+            className="searchForm__find-btn"
+            value="1"
+            form="searchForm"
+          ></button>
+        </div>
+      </div>
+      <FilterCheckbox />
+    </form>  */
