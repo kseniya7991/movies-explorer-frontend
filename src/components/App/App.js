5c5f48a0fc66a57f026c8e5ed6f460ad36eef79c
Kseniya St <kseniya7991@gmail.com>
2021-08-22 18:51:34 +0300

    disabled redirect

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import { useHistory } from 'react-router';
@@ -32,7 +32,7 @@ function App() {
   const [statusInfoPopup, setStatusInfoPopup] = useState(false);
   const [message, setMessage] = useState('');
 
-  function tokenCheck() {
+  /*   function tokenCheck() {
     const token = localStorage.getItem('token');
     if (token) {
       setLoggedIn(true);
@@ -42,7 +42,7 @@ function App() {
   useEffect(() => {
     tokenCheck();
   }, []);
-
+ */
   /*   useEffect(() => {
     console.log(loggedIn);
     if (loggedIn) {
