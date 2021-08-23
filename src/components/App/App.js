c8991a82bd5477860ca8ad84dbab2d09c947a755
Kseniya St <kseniya7991@gmail.com>
2021-08-23 21:45:39 +0300

    add get saved movies function

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -53,7 +53,7 @@ function App() {
     Promise.all([mainApi.getUser(), mainApi.getSavedMovies()])
       .then(([userData, moviesData]) => {
         setCurrentUser(userData.user);
-        console.log(moviesData);
+        console.log(moviesData, moviesData.movies);
         setSavedMovies(moviesData);
       })
       .catch((err) => console.log(err));
