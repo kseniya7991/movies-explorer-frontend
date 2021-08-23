ece73e9f211b6d53657fa60eb09bedb3f12ed7f1
Kseniya St <kseniya7991@gmail.com>
2021-08-23 22:21:43 +0300

    add get saved movies function

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -4,9 +4,11 @@ import SearchForm from '../SearchForm/SearchForm';
 import './SavedMovies.css';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import SavedMoviesContext from '../../contexts/SavedMoviesContext';
+import CurrentUserContext from '../../contexts/CurrentUserContext';
 /* import EmptyMoviesList from '../EmptyMoviesList/EmptyMoviesList'; */
 
 function SavedMovies() {
+  const currentUser = React.useContext(CurrentUserContext);
   const savedMovies = React.useContext(SavedMoviesContext);
 
   const [isListEmpty, setIsListEmpty] = useState(false);
@@ -15,6 +17,7 @@ function SavedMovies() {
     if (!savedMovies) {
       setIsListEmpty(true);
     }
+    savedMovies.filter((movie) => movie.owner === currentUser._id);
   }, []);
 
   console.log(savedMovies);
