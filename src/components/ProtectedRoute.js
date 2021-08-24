2624877acf6bd9fa13a5009553c2f38eabf53561
Kseniya St <kseniya7991@gmail.com>
2021-08-24 17:43:32 +0300

    add go back on 404 page

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -3,9 +3,9 @@ import { Route, Redirect } from 'react-router-dom';
 
 const ProtectedRoute = ({ component: Component, ...props }) => {
   console.log(props.loggedIn);
-  (
+  return (
   <Route>
-    {() => (props.loggedIn === true ? <Component {...props} /> : <Redirect to="/movies" />)}
+    {() => (props.loggedIn === true ? <Component {...props} /> : <Redirect to="/" />)}
   </Route>
   );
 };
