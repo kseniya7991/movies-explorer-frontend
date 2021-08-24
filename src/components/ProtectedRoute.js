2d2c1af1a03663650bf7ce1ec35be3abc4c17555
Kseniya St <kseniya7991@gmail.com>
2021-08-24 21:24:47 +0300

    get user from api by token

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-const ProtectedRoute = ({ component: Component, ...props }) => {
-  (
+const ProtectedRoute = ({ component: Component, ...props }) => (
   <Route>
     {() => (props.loggedIn === true ? <Component {...props} /> : <Redirect push to="/" />)}
   </Route>
-  );
-};
+);
 
 export default ProtectedRoute;
