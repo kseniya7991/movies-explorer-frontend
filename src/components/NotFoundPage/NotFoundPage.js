53e60fad50b4e509a50dbaa7c90664d10a9cf1e2
Kseniya St <kseniya7991@gmail.com>
2021-08-24 17:07:55 +0300

    add go back on 404 page

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { useHistory } from 'react-router';
 
 import './NotFoundPage.css';
 
 function NotFoundPage() {
   const history = useHistory();
-  console.log(history);
 
   return (
     <section className="notFoundPage">
       <p className="notFoundPage__error">404</p>
       <p className="notFoundPage__text">Страница не найдена</p>
-      <Link to="" className="notFoundPage__previous-link">
+      <button onClick={() => history.goBack()} className="notFoundPage__previous-link">
         Назад
-      </Link>
+      </button>
     </section>
   );
 }
