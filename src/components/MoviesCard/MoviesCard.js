579ae4ee410f59f5f21f80d114088eacc7c9548f
Kseniya St <kseniya7991@gmail.com>
2021-08-26 15:22:01 +0300

    get saved movies when the page is refreshed

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 /* import SavedMoviesContext from '../../contexts/SavedMoviesContext'; */
 
 import './MoviesCard.css';
