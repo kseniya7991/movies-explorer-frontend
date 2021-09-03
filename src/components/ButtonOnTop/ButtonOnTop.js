5d754021e50d117baa3e1d0166b54fcd6876a0b7
Kseniya St <kseniya7991@gmail.com>
2021-09-03 09:47:35 +0300

    add on top button

--- src/components/ButtonOnTop/ButtonOnTop.js
+++ src/components/ButtonOnTop/ButtonOnTop.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './ButtonOnTop.css';
 
 function ButtonOnTop() {
+  const [isEnabled, setIsEnabled] = useState(false);
+
+  function showButton() {
+    if (window.pageYOffset > 100) {
+      setIsEnabled(true);
+    }
+  }
+  //
+  function handleOnTop() {
+    window.scrollTo(0, 0);
+  }
+
+  // When scrolling, we run the function
+  window.onscroll = showButton;
+
   return (
-    <a href="#top" className="buttonOnTop">
-    </a>
+    <button
+    className={`buttonOnTop ${isEnabled ? 'buttonOnTop_active' : ''}`}
+    onClick={handleOnTop}
+    >
+    </button>
   );
 }
 
