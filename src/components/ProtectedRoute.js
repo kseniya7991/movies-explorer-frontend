4ebde09020c8ecf60f0a9a4ba419ae112c21cab9
Kseniya St <kseniya7991@gmail.com>
2021-09-05 15:11:26 +0300

    change useState loggedIn

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 const ProtectedRoute = ({ component: Component, ...props }) => {
-  if (props.loggedIn == null) {
+  if (props.loggedIn !== null) {
     return (
   <Route>
     {() => (props.loggedIn === true ? <Component {...props} /> : <Redirect push to="/" />)}
